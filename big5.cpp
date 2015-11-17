3a75a219c01324fdd440538397da7131c95a7a0c
N. Streifling <N.Streifling@gmail.com>
2015-11-17 09:54:00 -0800

    added assignment operators.

--- big5.cpp
+++ big5.cpp
@@ -13,15 +13,28 @@ public:
 	C(C &&c);
 	~C();
 	
+	C& operator=(const C &c);
+	C& operator=(C &&c);
+	
+	friend
+	ostream& operator<<(ostream& sout, const C c);
+	
 protected:
 	
 	const int _size;
 	int*      _arr;
 };
 
+C makeC()
+{
+	C c(6);
+	
+	return c;
+}
+
 int main()
 {
-	cout << "Hello world" << endl;
+	cout << makeC() << endl;
 	return 0;
 }
 
@@ -36,6 +49,9 @@ C::C(const int size) :
 _size(size),
 _arr(new int[size])
 {
+	for(int j = 0; j < _size; j++)
+		_arr[j] = j;
+	
 	cout << "Initial Constructor: " << _arr << endl;
 }
 
@@ -63,4 +79,39 @@ C::~C()
 		delete [] _arr;
 	
 	cout << "Destructor: " << _arr << endl;
+}
+
+C& C::operator=(const C &c)
+{
+	cout << "Copy Assignment Operator" << endl;
+	
+	int* mySize = const_cast<int*>(&_size);
+	
+	if(_arr != NULL)
+		delete [] _arr;
+	
+	*mySize = c._size;
+	_arr    = new int[c._size];
+	memcpy(_arr, c._arr, sizeof(int) * c._size);
+	
+	return *this;
+}
+
+C& C::operator=(C &&c)
+{
+	cout << "Move Assignment Operator" << endl;
+	
+	int* mySize = const_cast<int*>(&_size);
+	*mySize     = c._size;
+	_arr        = c._arr;
+	c._arr      = NULL;
+	
+	return *this;
+}
+
+ostream& operator<<(ostream& sout, const C c)
+{
+	for(int j = 0; j < c._size; j++)
+		sout << c._arr[j] << ' ';
+	return sout;
 }
